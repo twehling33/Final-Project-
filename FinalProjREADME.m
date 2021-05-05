f2b2d3cff23094b030dcd72ea3b53699b7c04dde
twehling33 <83328323+twehling33@users.noreply.github.com>
2021-05-05 16:01:10 -0500

    Add files via upload

---
+++ FinalProjREADME.m
@@ -0,0 +1,3 @@
+To run finalPlot2d all you have to do is enter any trig function you wish to plot inside the paranthesis at line six. If you also wish to find the exact value of the trig function at a specific x value, enter in the trig function at line 38 with x replaced with whatever value is desired. Also, if you wish to graph a trig function with an amplitude higher than one than I reccomend changing the y lims on line 32.  
+
+Technically you can actually plot any function using this graph but it is currently optimized for sin and cos functions. If you do want to graph other functions then I would recommend changing the y axes lims on line 32. 
